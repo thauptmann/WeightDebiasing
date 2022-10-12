ca22ce7e19cc46553cd6bc8c6b8b212854d527bd
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-10-12 10:28:03 +0200

    Fix experiment

--- src/methods/ada_debiasing.py
+++ src/methods/ada_debiasing.py
@@ -7,7 +7,7 @@ from tqdm.auto import tqdm
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 
-number_of_iterations = 100
+number_of_iterations = 50
 param_grid = {"max_depth": [1, 2], "min_samples_split": [2, 5]}
 
 
@@ -17,7 +17,7 @@ def ada_debiasing_weighting(N, R, columns, number_of_splits, *args, **kwargs):
     weights_R = (np.ones(len(R)) / len(R)) * weight_relation
     learning_rate = 0.5
 
-    for i in tqdm(range(number_of_iterations)):
+    for _ in tqdm(range(number_of_iterations)):
         prediction_n = train_weighted_tree(
             N, R, number_of_splits, columns, np.concatenate([weights_N, weights_R])
         )
@@ -33,9 +33,13 @@ def ada_debiasing_weighting(N, R, columns, number_of_splits, *args, **kwargs):
         )
 
         tmp = np.power(math.e, samples_alpha)
-        weights_N = weights_N * tmp
-        weights_N = weights_N / sum(weights_N)
-        learning_rate *= 0.95
+        new_weights_N = weights_N * tmp
+        new_weights_N = new_weights_N / sum(new_weights_N)
+        if (new_weights_N == weights_N).all():
+            break
+        else:
+            weights_N = new_weights_N
+        learning_rate *= 0.99
 
     return weights_N
 
@@ -48,6 +52,7 @@ def train_weighted_tree(N, R, number_of_splits, columns, weights):
 
 
 def train_tree(X_train, y_train, weights, number_of_splits):
+    weights = np.nan_to_num(weights)
     decision_stump = GridSearchCV(
         DecisionTreeClassifier(), param_grid, n_jobs=-1, cv=number_of_splits
     )
