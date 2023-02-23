24ad9f9d7ed5e6066ad75008e294e5d32bf1a400
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2023-02-23 10:59:02 +0100

    Remove random weighting

--- src/methods/random.py
+++
@@ -1,7 +0,0 @@
-from numpy.random import default_rng
-
-def random_weighting(N, *args, **attributes):
-    rng = default_rng()
-    weights = rng.uniform(size=len(N))
-    weights = weights / sum(weights)
-    return weights
