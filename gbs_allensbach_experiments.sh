7df4aeb879f81d6573d8b89d2e3312a67935fb35
Tony Hauptmann <hauptmann@mirzachani.Informatik.Uni-Mainz.DE>
2023-06-27 16:52:02 +0200

    Fix a wrong call

--- gbs_allensbach_experiments.sh
+++ gbs_allensbach_experiments.sh
@@ -1,4 +1,4 @@
 for METHOD in uniform logistic_regression neural_network_mmd_loss kmm adaDeBoost mrs
 do
-    python src/run_weighting_experiment.py --dataset gbs_allensbach --method $METHOD 
+    python src/weighting_experiment.py --dataset gbs_allensbach --method $METHOD 
 done
