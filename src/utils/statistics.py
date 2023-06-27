3f3c799e741d4d2421c16adcf47b209fdb1f97da
Tony Hauptmann <hauptmann@mirzachani.Informatik.Uni-Mainz.DE>
2023-06-27 18:21:59 +0200

    Update a deprecated method

--- src/utils/statistics.py
+++ src/utils/statistics.py
@@ -6,14 +6,14 @@ def logistic_regression(allensbach_gbs, weights):
     y = allensbach_gbs["Wahlteilnahme"]
     X = allensbach_gbs["Resilienz"]
     X = sm.add_constant(X)
-    model_gbs = sm.GLM(y, X, family=sm.families.Binomial(sm.families.links.logit()))
+    model_gbs = sm.GLM(y, X, family=sm.families.Binomial(sm.families.links.Logit()))
     results_gbs = model_gbs.fit()
     lr_pvalue_gbs = results_gbs.pvalues[1]
 
     model_all = sm.GLM(
         y,
         X,
-        family=sm.families.Binomial(sm.families.links.logit()),
+        family=sm.families.Binomial(sm.families.links.Logit()),
         freq_weights=weights,
     )
     results_weighted = model_all.fit()
