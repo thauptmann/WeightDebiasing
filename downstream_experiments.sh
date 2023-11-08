51ec4454a5164ab06dacfddceaa87fcdf2424b6f
Tony Hauptmann <65155653+thauptmann@users.noreply.github.com>
2023-11-08 14:57:17 +0100

    Fix script

--- downstream_experiments.sh
+++ downstream_experiments.sh
@@ -4,17 +4,10 @@ for METHOD in uniform logistic_regression kmm soft-mrs mrs
 do
     for DATASET in breast_cancer loan_prediction hr_analytics folktables_income folktables_employment 
     do
-        # for BIAS_TYPE in less_negative_class less_positive_class mean_difference
-        for BIAS_TYPE in less_negative_class less_positive_class
+        for BIAS_TYPE in less_negative_class less_positive_class mean_difference
             do
             python src/weighting_experiment.py --dataset $DATASET --method $METHOD \
             --bias_type $BIAS_TYPE --number_of_repetitions $NUMBER_OF_REPETETIONS  
             done
     done
 done
-
-#for DATASET in breast_cancer loan_prediction hr_analytics folktables_income folktables_employment 
-#do
-#    python src/weighting_experiment.py --dataset $DATASET --method uniform --bias_type none \
-#    --number_of_repetitions $NUMBER_OF_REPETETIONS  
-#done
